c6e140628e33b363a907b4fec4bcbf4c4c2d04a6
Marino <marinogudmundsson@gmail.com>
2018-09-04 17:39:55 +0000

    *** CLASSIFIED CHANGES ***

--- max_int.py
+++ max_int.py
@@ -19,6 +19,7 @@ print("The maximum is", max_int)    # Do not change this line
 
 
 
+
 """ Design an algorithm that finds the maximum positive integer input by a user.  The user repeatedly inputs numbers until a negative value is entered.
  
 Make sure that you write up the algorithm before starting to code.
